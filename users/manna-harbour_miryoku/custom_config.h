e9dffbbdc71dc246d0a5c8a0c47352afcd1b0d8e
Mubeen Khan <8978476+mubeenkhan94@users.noreply.github.com>
2024-02-03 23:56:38 -0600

    try to make it work

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,17 +5,42 @@
 
 #pragma once
 
-#define MASTER_LEFT
-
-#undef USE_I2C
 #undef SSD1306OLED
-
-#define USE_SERIAL_PD2
 #define OLED_FONT_H "users/manna-harbour_miryoku/glcdfont.c"
-
-#define MIRYOKU_ALPHAS = MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH_SWAP_SPACE
-#define MIRYOKU_EXTRA = MIRYOKU_ALTERNATIVES_BASE_QWERTY_SWAP_SPACE
-#define MIRYOKU_CLIPBOARD = MAC
 
 #define SPLIT_WPM_ENABLE   // Enable WPM across split keyboards (+268).
 #define SPLIT_OLED_ENABLE  // Sync on/off OLED state between halves (+100).
+
+#define KC_LK   C(G(KC_Q)) // lock Mac computer
+#define KC_CLP  S(G(KC_V)) // launch Alfred clipboard manager
+#define KC_NAV  LT(U_NAV, KC_QUOT)
+#define KC_NML  MO(U_NUM)
+#define KC_SYM  MO(U_SYM)
+#define KC_FUN  MO(U_FUN)
+
+#define MIRYOKU_MAPPING( \
+      K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09, \
+      K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19, \
+      K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29, \
+      N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39 \
+) \
+LAYOUT_split_3x6_3( \
+KC_NML,  K00,  K01,  K02,  K03,  K04,      K05,  K06,  K07,  K08,  K09,  KC_CLP, \
+KC_SYM,  K10,  K11,  K12,  K13,  K14,      K15,  K16,  K17,  K18,  K19,  KC_NAV, \
+KC_FUN,  K20,  K21,  K22,  K23,  K24,      K25,  K26,  K27,  K28,  K29,  KC_LK , \
+                  K32,  K33,  K34,         K35,  K36,  K37 \
+)
+
+#define MIRYOKU_LAYER_BASE \
+KC_Q,              KC_W,             KC_F,               KC_P,              KC_B,               KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,           \
+LGUI_T(KC_A),      LALT_T(KC_R),     LCTL_T(KC_S),       LSFT_T(KC_T),      KC_G,               KC_M,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_I),      LGUI_T(KC_O),      \
+LT(U_BUTTON,KC_Z), ALGR_T(KC_X),     KC_C,               KC_D,              KC_V,               KC_K,              KC_H,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH),\
+U_NP,              U_NP,             LT(U_MEDIA,KC_ESC), LT(U_NAV,KC_BSPC), LT(U_MOUSE,KC_TAB), LT(U_SYM,KC_ENT),  LT(U_NUM,KC_SPC),  LT(U_FUN,KC_DEL),  U_NP,              U_NP
+
+#define MIRYOKU_LAYER_EXTRA \
+KC_Q,              KC_W,             KC_E,               KC_R,              KC_T,               KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
+LGUI_T(KC_A),      LALT_T(KC_S),     LCTL_T(KC_D),       LSFT_T(KC_F),      KC_G,               KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),   \
+LT(U_BUTTON,KC_Z), ALGR_T(KC_X),     KC_C,               KC_V,              KC_B,               KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH),\
+U_NP,              U_NP,             LT(U_MEDIA,KC_ESC), LT(U_NAV,KC_BSPC), LT(U_MOUSE,KC_TAB), LT(U_SYM,KC_ENT),  LT(U_NUM,KC_SPC),  LT(U_FUN,KC_DEL),  U_NP,              U_NP
+
+#define MIRYOKU_CLIPBOARD = MAC
