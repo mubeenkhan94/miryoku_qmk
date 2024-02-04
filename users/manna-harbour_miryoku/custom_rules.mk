2e535d49408aa8d2b694ad6d98221cf4e210b5ae
Mubeen Khan <8978476+mubeenkhan94@users.noreply.github.com>
2024-02-03 21:14:03 -0600

    reduce memory usage

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,7 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
--- users/manna-harbour_miryoku/rules.mk
+++ users/manna-harbour_miryoku/rules.mk
@@ -1,7 +1,7 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
-MOUSEKEY_ENABLE = yes
+MOUSEKEY_ENABLE = no
 EXTRAKEY_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
 TAP_DANCE_ENABLE = yes
