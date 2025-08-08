9ff09a864ce981bd130644ef500b02cd5c6ecaf2
Mubeen Khan <8978476+mubeenkhan94@users.noreply.github.com>
2025-08-07 20:03:42 -0500

    swap ctrl and command

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -51,13 +51,13 @@ LAYOUT( \
 
 #define MIRYOKU_LAYER_BASE \
 KC_Q,              KC_W,             KC_F,               KC_P,              KC_B,               KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,           \
-LGUI_T(KC_A),      LALT_T(KC_R),     LCTL_T(KC_S),       LSFT_T(KC_T),      KC_G,               KC_M,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_I),      LGUI_T(KC_O),      \
+LCTL_T(KC_A),      LALT_T(KC_R),     LGUI_T(KC_S),       LSFT_T(KC_T),      KC_G,               KC_M,              LSFT_T(KC_N),      LGUI_T(KC_E),      LALT_T(KC_I),      LCTL_T(KC_O),      \
 LT(U_BUTTON,KC_Z), ALGR_T(KC_X),     KC_C,               KC_D,              KC_V,               KC_K,              KC_H,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH),\
 U_NP,              U_NP,             LT(U_MEDIA,KC_ESC), LT(U_NAV,KC_BSPC), LT(U_MOUSE,KC_TAB), LT(U_SYM,KC_ENT),  LT(U_NUM,KC_SPC),  LT(U_FUN,KC_DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_EXTRA \
 KC_Q,              KC_W,             KC_E,               KC_R,              KC_T,               KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
-LGUI_T(KC_A),      LALT_T(KC_S),     LCTL_T(KC_D),       LSFT_T(KC_F),      KC_G,               KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),   \
+LCTL_T(KC_A),      LALT_T(KC_S),     LGUI_T(KC_D),       LSFT_T(KC_F),      KC_G,               KC_H,              LSFT_T(KC_J),      LGUI_T(KC_K),      LALT_T(KC_L),      LCTL_T(KC_QUOT),   \
 LT(U_BUTTON,KC_Z), ALGR_T(KC_X),     KC_C,               KC_V,              KC_B,               KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH),\
 U_NP,              U_NP,             LT(U_MEDIA,KC_ESC), LT(U_NAV,KC_BSPC), LT(U_MOUSE,KC_TAB), LT(U_SYM,KC_ENT),  LT(U_NUM,KC_SPC),  LT(U_FUN,KC_DEL),  U_NP,              U_NP
 
@@ -69,13 +69,13 @@ U_NP,              U_NP,              KC_ESC,            KC_BSPC,           KC_T
 
 #define MIRYOKU_LAYER_NAV \
 TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,               U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,               CW_TOGG,           KC_LEFT,           KC_DOWN,           KC_UP,             KC_RGHT,           \
+KC_LCTL,           KC_LALT,           KC_LGUI,           KC_LSFT,           U_NA,               CW_TOGG,           KC_LEFT,           KC_DOWN,           KC_UP,             KC_RGHT,           \
 U_NA,              KC_ALGR,           TD(U_TD_U_NUM),    TD(U_TD_U_NAV),    U_NA,               KC_INS,            KC_HOME,           KC_PGDN,           KC_PGUP,           KC_END,            \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,               KC_ENT,            KC_SPC,            KC_DEL,            U_NP,              U_NP
 
 #define MIRYOKU_LAYER_FUN \
 KC_F12,            KC_F7,             KC_F8,             KC_F9,             KC_PSCR,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
-KC_F11,            KC_F4,             KC_F5,             KC_F6,             KC_SCRL,           U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+KC_F11,            KC_F4,             KC_F5,             KC_F6,             KC_SCRL,           U_NA,              KC_LSFT,           KC_LGUI,           KC_LALT,           KC_LCTL,           \
 KC_F10,            KC_F1,             KC_F2,             KC_F3,             KC_PAUS,           U_NA,              TD(U_TD_U_FUN),    TD(U_TD_U_MEDIA),  KC_ALGR,           U_NA,              \
 U_NP,              U_NP,              KC_APP,            KC_BSPC,           KC_TAB,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
